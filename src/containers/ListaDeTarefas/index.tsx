6879a464cdf315a6f7327874e8577ac8919e167a
Rafael Almeida <dealmeida.silva@hotmail.com>
2024-04-29 07:59:47 -0300

    finalizado o textarea

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,7 +1,30 @@
+import Tarefa from '../../components/Tarefa'
+import { Container } from './styles'
+
 const ListaDeTarefas = () => (
-  <main>
+  <Container>
     <p>2 tarefas marcadas como: &quot;categoria &ldquo; &quot;termo&ldquo;</p>
-  </main>
+    <ul>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+    </ul>
+  </Container>
 )
 
 export default ListaDeTarefas
